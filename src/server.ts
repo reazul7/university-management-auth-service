140d00491b6e129b1a8914d344bdecbbf0daef83
reazul7 <islamreazul7777@gmail.com>
2023-06-01 00:00:25 +0600

    DB connection successfully with whitelist ip

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
 async function main() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        await mongoose.connect(config.database_url as string);
         console.log(`Database is connected successfully`);
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+        })
     } catch (err) {
         console.log(`Failed to connect database`, err)
     }
 }
+main();
