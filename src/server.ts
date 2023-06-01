703a51631de09dd00a99a7a22362c8f7d430a6a0
reazul7 <islamreazul7777@gmail.com>
2023-06-01 15:51:29 +0600

    prettier setup complete

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import config from "./config";
 import app from "./app";
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database is connected successfully`);
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database is connected successfully`);
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log(`Failed to connect database`, err)
-    }
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`Failed to connect database`, err);
+  }
 }
 main();
