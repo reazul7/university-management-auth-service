0971b8dae10accbfbad8582a7780759e3ac195b1
reazul7 <islamreazul7777@gmail.com>
2023-06-01 23:29:44 +0600

    successfully create a user with default password and id will be incremented+1

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import usersRouter from "./app/modules/users/users.route";
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+// Application routes
+app.use("/api/v1/users/", usersRouter);
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Working Successfully");
 });
 
 export default app;
