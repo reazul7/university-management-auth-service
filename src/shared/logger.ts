624bc4665b807920fd7cd3e94dc131917bb519b0
reazul7 <islamreazul7777@gmail.com>
2023-06-06 00:31:55 +0600

    customize logger

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,26 +1,39 @@
-import winston from "winston";
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, label, printf } = format;
 import path from "path";
 
-const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp);
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+  const month = new Intl.DateTimeFormat("en", { month: "short" }).format(date);
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  const newFormatDate = day + " " + month + " " + year;
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${newFormatDate} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: combine(label({ label: "right meow!" }), timestamp(), myFormat),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), "logs", "winston", "success.log"),
       level: "info",
     }),
   ],
 });
 
-const errorLogger = winston.createLogger({
+const errorLogger = createLogger({
   level: "error",
-  format: winston.format.json(),
+  format: combine(label({ label: "right meow!" }), timestamp(), myFormat),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), "logs", "winston", "error.log"),
       level: "error",
     }),
